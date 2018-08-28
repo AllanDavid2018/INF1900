f3cad026628bc69660c4ac0a56270ca72cfd855b
David Dratwa <dadra@l3818-17.info.polymtl.ca>
2018-08-28 16:30:56 -0400

    modification pour le TP2

--- TP1/main.cpp
+++ TP1/main.cpp
@@ -5,34 +5,140 @@
  * Description: Ceci est un exemple simple de programme 
  * Version: 1.1
  */
-
+#define F_CPU 8000000
+#include <util/delay.h>
 
 #include <avr/io.h> 
+
+ 
+
+void allumerAmbre (int tempsSeconde) {
+	 for (int i = 0; i < tempsSeconde * 1000; i++){
+		PORTA = 0x01;
+			_delay_ms (1);
+		PORTA= 0b10; 
+		_delay_ms (1); 
+	  
+		}
+		PORTA = 0b00;
+}
+void allumerRouge(uint8_t tempsSeconde) {
+	 PORTA = 0b10;
+	 for (uint8_t i = 0; i < tempsSeconde; i++) 
+	  _delay_ms (1000);
+	  PORTA = 0b00;
+  
+}
+
+
+void allumerVert(uint8_t tempsSeconde) {
+	 PORTA = 0b01;
+	for (uint8_t i = 0; i < tempsSeconde; i++) 
+	  _delay_ms (1000);
+	 PORTA = 0b00;
+}
+
+void allumerVert(){
+	PORTA = 0b01;
+
+}
+
+void allumerRouge(){
+	PORTA = 0b10;
+
+}
+
+void allumerAmbre() {
+	for (;;){
+		PORTA = 0x01;
+			_delay_ms (1);
+		PORTA= 0b10; 
+		_delay_ms (1); 
+		if (PIND == 0b00) 
+			break; 
+		}
+} 
+
+
+void eteindre() { 
+	PORTA = 0b00;
+}
+
+void estEteint(){
+	for(;;)
+	if(!(PIND & 0x04))
+		break;
+}
+
+bool estAppuye()
+{
+	if(PIND & 0x04)
+		_delay_ms (40); 
+	return PIND & 0x04;
+		
+}
 
 int main()
 {
   DDRA = 0xff; // PORT A est en mode sortie
   DDRB = 0xff; // PORT B est en mode sortie
   DDRC = 0xff; // PORT C est en mode sortie
-  DDRD = 0xff; // PORT D est en mode sortie
-  unsigned long compteur=0; // le compteur est initialise a 0.
-                            // c'est un compteur de 32 bits
+  DDRD = 0x00; // PORT D est en mode entree 
+      /*    
+    for (;;){
 
+		if ( PIND & 0x04 ) 
+			allumerAmbre();
+		else eteindre(); 
+			
+	}
+          
+                       
   for(;;)  // boucle sans fin
   {
-    compteur++;  // incremente le compteur
-                 // si le compteur est 0xffff ffff il revient a 0
-    PORTD = compteur;       // PORTD = 8 bits de large,
-                            // il prend les bits de 0 a 7 du compteur
-    PORTC = compteur >> 8;  // PORTC = 8 bits de large,
-                            // il prend les bits de 8 a 15 du compteur
-                            // en faisant un decallage de 8 bits.
-    PORTB = compteur >> 16; // PORTB = 8 bits de large,
-                            // il prend les bits de 16 a 23 du compteur
-                            // en faisant un decallage de 16 bits.
-    PORTA = compteur >> 24; // PORTA = 8 bits de large,
-                            // il prend les bits de 24 a 31 du compteur
-                            // en faisant un decallage de 24 bits.
+	
+		
+	
+	  allumerRouge(1);
+	  allumerVert(1);
+	  allumerAmbre(1);
+	  	
+    
   }
+  */
+	
+	uint8_t EtatCouleur=0;
+	bool estFerme=1;
+	for(;;)
+	{
+		if(estAppuye() && estFerme){
+			EtatCouleur++;
+			estFerme=0;
+}
+			
+		switch (EtatCouleur){
+		case 1 : 
+					allumerRouge(); 
+					break;
+		
+		case 2 : 
+				  allumerVert();
+				  break;
+				  
+		case 3 : allumerVert();
+				 _delay_ms (1);
+				 allumerRouge();
+				 _delay_ms (1);
+				  break;
+				   
+		case 4 : 
+					EtatCouleur=1;
+					break;
+
+		}
+if (!(PIND & 0x04))
+	estFerme=1;
+	}
+  
   return 0; 
 }

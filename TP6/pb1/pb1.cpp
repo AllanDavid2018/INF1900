a0fc9cb37913cdca961c7df10aae642fda06710a
Allan BEDDOUK <allanbeddouk@gmail.com>
2018-10-15 10:23:15 -0400

    Remise finale pb1 TP6

--- TP6/pb1/pb1.cpp
+++ TP6/pb1/pb1.cpp
@@ -6,32 +6,26 @@
  * @modified: 
  */
 
-#define F_CPU 8000000UL
+#define F_CPU 8000000
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
 void partirMinuterie ( uint16_t duree );
 
-/**
- * Light on the LED in green
- */
+
 void allumerVert()
 {
     PORTA = 0x2;
 }
 
-/**
- * Light on the LED in red
- */
+
 void allumerRouge()
 {
     PORTA = 0x1;
 }
 
-/**
- * Light off the LED
- */
+
 void eteindre()
 {
     PORTA = 0x0;
@@ -39,26 +33,21 @@ void eteindre()
 
 volatile uint8_t compteur;
 
-void msleep(int ms) {
-    for(int i = 0; i < ms; i++){
-        _delay_loop_2(2000);
-    }
-}
 
 ISR (TIMER1_COMPA_vect) {
-    compteur++;
+    compteur+=10;
 }
 
-ISR (INT0_vect) {
+ISR ( INT0_vect) {
 
     // Anti rebond
     _delay_ms(50);
+   
 
-    if (!(PIND & 0x04)) {
-        partirMinuterie(781);
-    }
+    if (!(PIND & 0x04)) 
+        partirMinuterie(F_CPU/1024);
+    
 
-    // Voir la note plus bas pour comprendre cette instruction et son role
     EIFR |= (1 << INTF0);
 }
 
@@ -66,11 +55,12 @@ void partirMinuterie ( uint16_t duree ) {
     compteur = 0;
     // mode CTC du timer 1 avec horloge divisee par 1024
     // interruption après la duree specifiee
+      
     TCNT1 = 0;
     OCR1A = duree;
     TCCR1A |= _BV(COM1A1) | _BV(COM1A0);
-    TCCR1B |= _BV(WGM12) | _BV(CS12) | _BV(CS10); // 00001101; WGM12 = CTC mode, 
-    TIMSK1 |= _BV(OCIE1A); //00000010;
+    TCCR1B |= _BV(WGM12) | _BV(CS12) | _BV(CS10); // 0b00001101; WGM12 = CTC mode, 
+    TIMSK1 |= _BV(OCIE1A); //0b00000010;
 }
 
 void arreterMinuterie() {
@@ -80,79 +70,65 @@ void arreterMinuterie() {
     TIMSK1 = 0;
 }
 
-void initialisation ( void ) {
+void initialisation () {
+// Désactive les interruptions
+    cli();
 
-    // Désactive les interruptions
-    cli ();
-
-    DDRA = 0x03; // Output A
-    DDRD = 0x00; // Input D
+  DDRA = 0xff; // PORT A est en mode sortie
+  DDRB = 0xff; // PORT B est en mode sortie
+  DDRC = 0xff; // PORT C est en mode sortie
+  DDRD = 0x00; // PORT D est en mode entree 
 
     // Permettre les interruptions externes
-    EIMSK |= _BV(INT0);
+    
+    EIMSK = _BV(INT0);
 
-    // Sensibiliser les interruptions externes
-    EICRA |= _BV(ISC01); // Falling edge
+    // Interruptions externes sont actives sur n'importe quel rising edge
+   
+    EICRA =_BV(ISC00);
 
-    // Réactive les interruptions
-    sei ();
+    // Reactive les interruptions
+    sei();
 }
 
 
-/**
- * The main function. It does the logic.
- * 
- * COMMENT BRANCHER LES FILS:
- * Del libre + sur PORTA<0>
- * Del libre - sur PORTA<1>
- * Output du bread board sur PORTD<2>
- * N'importe quel vcc et gnd de la carte sur le vcc et gnd du breadboard
- */
 int main()
 {
+	
     initialisation();
-
-    for(;;) {
-
-        do {
-
-        // attendre qu'une des deux variables soit modifiée
-        // par une ou l'autre des interruptions.
+	while (compteur <= 120 && (!(PIND & _BV(PORTD2)) || compteur == 0)); 
+    arreterMinuterie();
         
-        //(PIND & _BV(PORTD2)) == true if button is not pressed
-        // We want to interate as long as the button is pressed, if it has been pressed (compteur > 1)
-        } while (compteur <= 120 && (!(PIND & _BV(PORTD2)) || compteur == 0)); 
-        arreterMinuterie();
-        
-        // Une interruption s'est produite. Arreter toute
-        // forme d'interruption. Une seule reponse suffit.
-        cli ();
+	// Une interruption s'est produite. Arreter toute
+	// forme d'interruption. Une seule reponse suffit.
+	cli ();
 
-        // Blink green for 1/2 seconds, at 2 Hz
-        allumerVert();
-        msleep(500);
+	// Allume vert pour 1/2 seconde
+	allumerVert();
+	_delay_ms(500);
 
-        // Wait 2 seconds
-        eteindre();
-        msleep(2000);
+	// pause de 2 secondes
+	eteindre();
+	_delay_ms(2000);
 
-        // Blink red (counter/2) times at 2 Hz
-        for (uint8_t i = 0; i < compteur/2; i++) {
-            allumerRouge();
-            msleep(250);
-            eteindre();
-            msleep(250);
-        }
+	// Allume rouge (compteur/2) fois
+	for (uint8_t i = 0; i < compteur/2; i++) 
+	{
+		allumerRouge();
+		_delay_ms(250);
+		eteindre();
+		_delay_ms(250);
+	}
 
-        // Turn on green light for a second
-        allumerRouge();
-        msleep(1000);
-        eteindre();
-        compteur = 0;
+	// Turn on green light for a second
+	allumerVert();
+	_delay_ms(1000);
+	eteindre();
+	compteur = 0;
 
-    // Réactive les interruptions
-        sei ();
-    }
+	// Réactive les interruptions
+	sei ();
+    
 
     return 0; 
 }
